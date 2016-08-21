d0771045632c7a291013824c964680dac3b61b88
Luciano Martorella <mad_lux_it@yahoo.it>
2016-08-21 16:45:46 +0000

    - WIP protocol

---
+++ server/Home.Lib/Sinks/HalfDuplexLineSink.cs
@@ -0,0 +1,51 @@
+﻿using Lucky.Home.Serialization;
+using System;
+using System.Threading.Tasks;
+
+namespace Lucky.Home.Sinks
+{
+    /// <summary>
+    /// Sink for half-duplex serial line: 9600,N,1, 0.1 sec RX timeout
+    /// </summary>
+    [SinkId("SLIN")]
+    class HalfDuplexLineSink : SinkBase
+    {
+        private class Message
+        {
+            [SerializeAsDynArray]
+            public byte[] TxData;
+        }
+
+        private class Response
+        {
+            [SerializeAsDynArray]
+            public byte[] RxData;
+        }
+
+        public async Task<byte[]> SendReceive(byte[] txData)
+        {
+            Write(writer =>
+            {
+                writer.Write(new Message { TxData = txData });
+            });
+
+            // Wait 0.2 sec (192 words)
+            await Task.Delay(TimeSpan.FromSeconds(0.2));
+
+            byte[] data = null;
+            try
+            {
+                Read(reader =>
+                {
+                    data = reader.Read<Response>().RxData;
+                });
+            }
+            catch
+            {
+                return null;
+            }
+
+            return data;
+        }
+    }
+}
