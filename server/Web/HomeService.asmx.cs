73ec947d078bc6d7b1e8d6098a285a25ad124111
Luciano Martorella <mad_lux_it@yahoo.it>
2016-10-11 20:59:28 +0000

    - Added ASP.NET project

---
+++ server/Web/HomeService.asmx.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Script.Services;
+using System.Web.Services;
+
+namespace Web
+{
+    public class Data
+    {
+        public string Ret = "It really works now.";
+    }
+
+    [WebService(Namespace = "uri://luckysoft")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [ScriptService]
+    public class HomeService : WebService
+    {
+        [WebMethod]
+        public Data GetData()
+        {
+            return new Data();
+        }
+    }
+}
---
+++ server/Web/js/main.ts
@@ -0,0 +1,16 @@
+﻿/// <reference path="typings/angular.d.ts" />
+
+module home.app {
+
+    interface IScope extends ng.IScope {
+        works: string;
+    }
+
+    angular.module('home', []).controller('test', ['$scope', '$http', ($scope: IScope, $http: ng.IHttpService) => {
+        $scope.works = "Loading...";
+        $http.post('/HomeService.asmx/GetData', null).then(value => {
+            $scope.works = "Loaded: " + (<any>value.data).d.Ret;
+        });
+    }]);
+
+}
