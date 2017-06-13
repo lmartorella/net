18860340fa9c117486db3f0cbf2476611fb8c813
Luciano Martorella <mad_lux_it@yahoo.it>
2017-06-13 21:33:32 +0000

    - Added auto-backup of csv files

--- server/Home.Lib/Db/FsTimeSeries.cs
+++ server/Home.Lib/Db/FsTimeSeries.cs
@@ -1,4 +1,5 @@
-﻿using Lucky.Services;
+﻿using Lucky.Home.Notification;
+using Lucky.Services;
 using System;
 using System.IO;
 
@@ -35,6 +36,9 @@ namespace Lucky.Home.Db
         {
             lock (_fileName)
             {
+                var oldFileName = _fileName;
+
+                // Change filename, so open a new file
                 _fileName = new FileInfo(Path.Combine(_folder, fileName));
                 _lastPeriod = _currentPeriod;
                 _currentPeriod = new PeriodData<T>(start);
@@ -42,6 +46,9 @@ namespace Lucky.Home.Db
                 // Write CSV header
                 WriteLine(writer => writer.WriteLine("TimeStamp," + _header));
                 _logger.Log("Rotated", "date", start.Date);
+
+                // Now the old file is free to be copied in backup
+                CopyToBackup(oldFileName);
             }
         }
 
@@ -94,6 +101,42 @@ namespace Lucky.Home.Db
                 // In addition write on the CSV file
                 WriteLine(writer => writer.WriteLine(ts.ToString(_timeStampFormat) + "," + sample.ToCsv()));
             }
+        }
+
+        private void CopyToBackup(FileInfo oldFileName)
+        {
+            if (oldFileName.Exists)
+            {
+                var targetFile = GetBackupPath(oldFileName);
+                try
+                {
+                    oldFileName.CopyTo(targetFile.FullName);
+                }
+                catch (Exception exc)
+                {
+                    _logger.Exception(exc);
+                    Manager.GetService<INotificationService>().SendMail("File locked", "Cannot backup the db file: " + oldFileName.FullName + Environment.NewLine + "EXC: " + exc.Message);
+                }
+            }
+        }
+
+        private FileInfo GetBackupPath(FileInfo srcFileName)
+        {
+            DirectoryInfo targetDir = new DirectoryInfo(Path.Combine(srcFileName.Directory.FullName, "backup"));
+            if (!targetDir.Exists)
+            {
+                targetDir.Create();
+            }
+            string baseName = srcFileName.Name;
+            string targetFileName = baseName;
+            var targetFile = new FileInfo(Path.Combine(targetDir.FullName, targetFileName));
+            int c = 1;
+            while (targetFile.Exists)
+            {
+                targetFileName = baseName + string.Format(" ({0})", c++);
+                targetFile = new FileInfo(Path.Combine(targetDir.FullName, targetFileName));
+            }
+            return targetFile;
         }
     }
 }
