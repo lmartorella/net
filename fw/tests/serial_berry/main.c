2e745d12e76e44bdccc52d03d0078aa6a04bda31
Luciano Martorella <mad_lux_it@yahoo.it>
2017-07-31 18:17:09 +0000

    - WIP direct drive PL011

---
+++ fw/tests/serial_berry/main.c
@@ -0,0 +1,137 @@
+#define _GNU_SOURCE
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <errno.h> 
+
+enum {
+    REG_DR = 0,
+    REG_RSRECR = 4,
+    REG_FR = 0x18,
+    REG_IBRD = 0x24,
+    REG_FBRD = 0x28,
+    REG_LCRH = 0x2c,
+    REG_CR = 0x30,
+    REG_IFLS = 0x34,
+    REG_IMSC = 0x38,
+    REG_RIS = 0x3c,
+    REG_MIS = 0x40,
+    REG_ICR = 0x44,
+    REG_DMACR = 0x48
+};
+
+#define REG_FR_TXFE 0x80
+#define REG_FR_RXFF 0x40
+#define REG_FR_TXFF 0x20
+#define REG_FR_RXFE 0x10
+#define REG_FR_BUSY 0x08
+
+#define REG_LCRH_SPS   0x80
+#define REG_LCRH_WLEN8   0x60
+#define REG_LCRH_FEN   0x10
+#define REG_LCRH_EPS   0x04
+#define REG_LCRH_PEN   0x02
+
+#define REG_CR_RXE   0x200
+#define REG_CR_TXE   0x100
+#define REG_CR_UARTEN   0x01
+
+
+static void fatal(const char* err) {
+    fprintf(stderr, "%s: %d\n", err, errno);
+    exit(1);
+}
+
+static void* mmap_vmap;
+static uint32_t mmap_size;
+static int mmap_fdMem;
+static volatile uint32_t* mmap_ptr;
+
+static void mmap_create(uint32_t base, uint32_t len) {
+    uint32_t page_size = 0x4000;//getpagesize();
+    
+    // Round first byte to page boundary
+    uint32_t addr0 = (base / page_size) * page_size;
+    uint32_t offs = base - addr0;
+    // Round last byte to page boundary
+    uint32_t addr1 = ((base + len - 1) / page_size) * page_size;
+    mmap_size = (addr1 - addr0) + page_size;
+ 
+    mmap_fdMem = open("/dev/mem", O_RDWR | O_SYNC);
+    if (mmap_fdMem < 0) {
+       fatal("Mem open");
+    }
+    char* mmap_vmap = mmap(NULL, mmap_size, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_SHARED | MAP_LOCKED, mmap_fdMem, addr0);
+    if (mmap_vmap == MAP_FAILED) { 
+       fatal("Mem map");
+    }
+    mmap_ptr = (uint32_t*)(mmap_vmap + offs);
+}
+
+static void mmap_destroy() {
+    if (munmap(mmap_vmap, mmap_size) != 0) {
+       fatal("Mem unmap");
+    }
+    if (close(mmap_fdMem) != 0) {
+       fatal("Mem close");
+    }
+}
+
+static void mmap_wr(int regaddr, uint32_t value) {
+    mmap_ptr[regaddr] = value;
+}
+
+static uint32_t mmap_rd(int regaddr) {
+    return mmap_ptr[regaddr];
+}
+
+int main() {
+    const uint32_t pi_peri_phys = 0x20000000;
+    const uint32_t UART_BASE = (pi_peri_phys + 0x00201000);
+    const uint32_t UART_LEN = 0x90;
+
+    mmap_create(UART_BASE, UART_LEN);
+    
+    for (int i = 0; i < UART_LEN / 4; i++) {
+        printf("%08x ", mmap_rd(i));
+    }
+        
+    // 1. Disable UART
+    mmap_wr(REG_CR, 0);
+    // 2. Wait for the end of transmission or reception of the current character
+    while (mmap_rd(REG_FR) & REG_FR_BUSY);
+    // 3. Flush the transmit FIFO by setting the FEN bit to 0 in the Line Control Register, UART_LCRH
+    mmap_wr(REG_LCRH, 0);
+    // 4. Reprogram the Control Register, UART_CR.
+    double divisor = 700000000.0 / (16 * 19200.0);
+    mmap_wr(REG_FBRD, (int)divisor);
+    mmap_wr(REG_IBRD, (divisor - (int)divisor) * 64);
+    mmap_wr(REG_LCRH, REG_LCRH_SPS | REG_LCRH_WLEN8 | REG_LCRH_FEN | REG_LCRH_EPS | REG_LCRH_PEN);
+    // 5. Enable the UART.
+    mmap_wr(REG_CR, REG_CR_TXE | REG_CR_RXE | REG_CR_UARTEN);
+
+    // Disable interrupts
+    mmap_wr(REG_IMSC, 0);
+    mmap_wr(REG_ICR, 0);
+    
+    // Writes 0x1
+    mmap_wr(REG_DR, 1);
+    mmap_wr(REG_DR, 1);
+    mmap_wr(REG_DR, 1);
+    mmap_wr(REG_DR, 1);
+    mmap_wr(REG_DR, 1);
+
+    // 2. Wait for the end of transmission or reception of the current character
+    while (mmap_rd(REG_FR) & REG_FR_BUSY);
+    
+    return 0;
+}
+
