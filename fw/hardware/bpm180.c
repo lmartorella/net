1fba0ee6b384a250fd0831c044b0e89150c8a695
Luciano Martorella <mad_lux_it@yahoo.it>
2018-05-06 11:34:50 +0000

    - WIP pressure, wrong calcs

--- fw/hardware/bpm180.c
+++ fw/hardware/bpm180.c
@@ -33,7 +33,10 @@ static enum {
     STATE_RCV_CALIB_TABLE,
     STATE_ASK_TEMP,
     STATE_ASK_TEMP_2,
-    STATE_RCV_TEMP
+    STATE_RCV_TEMP,
+    STATE_ASK_PRESS,
+    STATE_ASK_PRESS_2,
+    STATE_RCV_PRESS
 } s_state;
 
 static BYTE s_buffer[16];
@@ -52,6 +55,10 @@ static struct {
     short mc;
     short md;
 } s_calibTable;
+// Uncompensate temp
+static short s_ut;
+// Uncompensate pressure
+static short s_up;
 
 void bpm180_init() {
     s_state = STATE_IDLE;
@@ -124,20 +131,70 @@ void bpm180_poll() {
             s_state = STATE_RCV_TEMP;
             break;
         case STATE_RCV_TEMP:
-            {
-                // invert temp
-                long uncompTemp = (s_buffer[0] << 8) + s_buffer[1];
+            // invert temp
+            s_ut = (s_buffer[0] << 8) + s_buffer[1];
 
-                // Calc true temp
-                long x1 = (uncompTemp - s_calibTable.ac6) * s_calibTable.ac5 / 0x8000l;
-                long x2 = (long)s_calibTable.mc * 0x800l / (x1 + s_calibTable.md);
-                long b5 = x1 + x2;
-                float t = (b5 + 8l) / 160.0f;  // Temp in C
-
-                // FOR ETH MASTER MODULE ONLY
-                sprintf(s_buffer, "%02d: Temp %5.1f", (s_counter++), t);
-                println(s_buffer);
+            // Ask pressure (min sampling)
+            s_buffer[0] = ADDR_MSG_CONTROL;
+            s_buffer[1] = MESSAGE_READ_PRESS_OSS_0;
+            i2c_sendReceive7(REG_WRITE, 2, s_buffer);
+            s_state = STATE_ASK_PRESS;
+            s_lastTime = TickGet();
+            break;
+        case STATE_ASK_PRESS:   
+            if ((TickGet() - s_lastTime) > (TICKS_PER_MSECOND * 5)) {
+                s_buffer[0] = ADDR_MSB;
+                i2c_sendReceive7(REG_WRITE, 1, s_buffer);
+                s_state = STATE_ASK_PRESS_2;
             }
+            break;
+        case STATE_ASK_PRESS_2:
+            // Read results
+            i2c_sendReceive7(REG_READ, 2, s_buffer);
+            s_state = STATE_RCV_PRESS;
+            break;
+        case STATE_RCV_PRESS:
+            // invert temp
+            s_up = (s_buffer[0] << 8) + s_buffer[1];
+
+            float temp, press;
+            long b5;
+            {
+                // Calc true temp
+                long x1 = ((long)s_ut - s_calibTable.ac6) * s_calibTable.ac5 / 0x8000l;
+                long x2 = (long)s_calibTable.mc * 2048l / (x1 + s_calibTable.md);
+                b5 = x1 + x2;
+                long t = (b5 + 8l) / 16l;  // Temp in 0.1C
+                temp = t / 10.0;
+            }
+            {
+                int oss = 0;
+                long b6 = b5 - 4000;
+                long x1 = ((long)s_calibTable.b2 * (b6 * b6 / 4096l)) / 2048l;
+                long x2 = (long)s_calibTable.ac2 * b6 / 2048l;
+                long x3 = x1 + x2;
+                long b3 = ((((long)s_calibTable.ac1 * 4 + x3) << oss) + 2) / 4l;
+                x1 = (long)s_calibTable.ac3 * b6 / 8192l;
+                x2 = ((long)s_calibTable.b1 * (b6 * b6 / 4096l)) / 65536l;
+                x3 = ((x1 + x2) + 2) / 4l;
+                unsigned long b4 = (long)s_calibTable.ac4 * (unsigned long)(x3 + 32768ul) / 32768l;
+                unsigned long b7 = ((unsigned long)s_up - b3) * (50000ul >> oss);
+                long p;
+                if (b7 < 0x80000000) {
+                   p = (b7 * 2) / b4; 
+                } else {
+                    p = (b7 / b4) * 2;
+                }
+                x1 = (p / 256) * (p / 256);
+                x1 = (x1 * 3038) / 65536l;
+                x2 = (-7357l * p) / 65536l;                
+                p = p + (x1 + x2 + 3791l) / 16l; // in Pascal
+                press = p / 100.0; // in hPa
+            }
+            
+            // FOR ETH MASTER MODULE ONLY
+            sprintf(s_buffer, "%4.1f'C %6.1fhPa", temp, press);
+            println(s_buffer);
                         
             // DONE!
             s_state = STATE_IDLE;
