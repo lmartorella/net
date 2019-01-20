0ff239128f8fdfc105a6a2d1d467daaba11203f0
Luciano Martorella <mad_lux_it@yahoo.it>
2019-01-20 17:35:48 +0000

    - Missing file

--- server/Home.Server.App/Sinks/DigitalInputArraySink.cs
+++ server/Home.Server.App/Sinks/DigitalInputArraySink.cs
@@ -15,25 +15,40 @@ namespace Lucky.Home.Sinks
     internal class DigitalInputArraySink : SinkBase
     {
         public TimeSpan PollPeriod = TimeSpan.FromSeconds(5);
-        private bool[] _status;
+        private bool[] _status = new bool[0];
 
         protected async override Task OnInitialize()
         {
             await base.OnInitialize();
-            Status = new bool[0];
             RunLoop();
         }
 
         // ReSharper disable once ClassNeverInstantiated.Local
-        private class ReadStatusResponse
+        private class ReadStatusResponse : ISerializable
         {
-            public ushort SwitchCount;
+            /// <summary>
+            /// Number of switches
+            /// </summary>
+            public int SwitchesCount; 
 
             /// <summary>
             /// Switch data in bytes 
             /// </summary>
-            [SerializeAsDynArray]
             public byte[] Data;
+
+            public async Task Deserialize(Func<int, Task<byte[]>> feeder)
+            {
+                byte[] size = await feeder(1);
+                // Size is in bits
+                SwitchesCount = size[0];
+                int len = (SwitchesCount - 1) / 8 + 1;
+                Data = await feeder(len);
+            }
+
+            public byte[] Serialize()
+            {
+                throw new NotImplementedException();
+            }
         }
 
         public bool[] Status
@@ -60,7 +75,7 @@ namespace Lucky.Home.Sinks
                         if (lastData != null && !resp.Data.SequenceEqual(lastData))
                         {
                             // Something changed
-                            int swCount = Math.Min(resp.SwitchCount, resp.Data.Length * 8);
+                            int swCount = Math.Min(resp.SwitchesCount, resp.Data.Length * 8);
                             var ret = new bool[swCount];
                             for (int i = 0; i < swCount; i++)
                             {
