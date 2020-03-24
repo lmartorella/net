790a2c0a8ad1a4fd3b962cad5e2f1a727e93a029
Luciano Martorella <mad_lux_it@yahoo.it>
2020-03-24 18:54:05 +0100

    - Fixed raspberry netmaster build

--- samples/netmaster/sinks.c
+++ samples/netmaster/sinks.c
@@ -1,6 +1,11 @@
 #include "../../src/nodes/pch.h"
 #include "../../src/nodes/sinks.h"
 
+const char* const SINK_IDS = SINK_SYS_ID;
+const int SINK_IDS_COUNT = 1;
+const SinkFunction const sink_writeHandlers[] = { sys_write };
+const SinkFunction const sink_readHandlers[] = { sys_read };
+
 void sinks_init() {
 }
 
