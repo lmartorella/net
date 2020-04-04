ea91a8579abe9f8c5ddb664a8c3a8850eda90302
Luciano Martorella <mad_lux_it@yahoo.it>
2020-04-04 14:37:58 +0200

    - Added pump logger - Refactored the big Garden device file in sub classes

---
+++ src/server/Home.Common/IO/FIleWatcher.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace Lucky.Home.IO
+{
+    /// <summary>
+    /// Wrapper around <see cref="FileSystemWatcher"/>, with debounce
+    /// </summary>
+    public class FileWatcher : IDisposable
+    {
+        private Timer _debounceTimer;
+        private FileSystemWatcher _cfgFileObserver;
+
+        public FileWatcher(FileInfo fileInfo)
+        {
+            _cfgFileObserver = new FileSystemWatcher(fileInfo.DirectoryName, fileInfo.Name);
+            _cfgFileObserver.Changed += (o, e) => Debounce(() => Changed?.Invoke(this, EventArgs.Empty));
+            _cfgFileObserver.NotifyFilter = NotifyFilters.LastWrite;
+            _cfgFileObserver.EnableRaisingEvents = true;
+        }
+
+        public event EventHandler Changed;
+
+        /// <summary>
+        /// Used to read config when the FS notifies changes
+        /// </summary>
+        private void Debounce(Action handler)
+        {
+            // Event comes two time (the first one with an empty file)
+            if (_debounceTimer == null)
+            {
+                _debounceTimer = new Timer(o =>
+                {
+                    _debounceTimer = null;
+                    handler();
+                }, null, 1000, Timeout.Infinite);
+            }
+        }
+
+        public void Dispose()
+        {
+            _cfgFileObserver.Dispose();
+        }
+    }
+}
