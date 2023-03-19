2f14ff35002f53e9e67e86d37f792c1324c4fbe2
Luciano Martorella <mad_lux_it@yahoo.it>
2023-03-19 18:25:55 +0100

    - Adding common service to replace the in-memory IPC pipe with the web gui

---
+++ src/server/Home.Common/Services/MqttService.cs
@@ -0,0 +1,48 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using System;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace Lucky.Home.Services
+{
+    [DataContract]
+    public class RpcRequest
+    {
+    }
+
+    [DataContract]
+    public class RpcResponse
+    {
+        [DataMember(Name = "error")]
+        public string Error { get; set; }
+    }
+
+    public class MqttService : ServiceBase
+    {
+        private readonly MqttFactory mqttFactory;
+        private readonly IMqttClient mqttClient;
+
+        public MqttService()
+        {
+            mqttFactory = new MqttFactory();
+            mqttClient = mqttFactory.CreateMqttClient();
+            mqttClient.ConnectAsync(new MqttClientOptions());
+        }
+
+        public void SubscribeRpc<TReq, TResp>(string topic, Func<TReq, Task<TResp>> handler)
+        {
+            var mqttSubscribeOptions = mqttFactory.CreateSubscribeOptionsBuilder().WithTopicFilter(f => f.WithTopic(topic)).Build();
+            mqttClient.SubscribeAsync(mqttSubscribeOptions);
+            mqttClient.ApplicationMessageReceivedAsync += async (MqttApplicationMessageReceivedEventArgs args) =>
+            {
+                if (args.ApplicationMessage.Topic == topic && args.ApplicationMessage.ResponseTopic != null)
+                {
+                    var result = await handler(args);
+                    args.IsHandled = true;
+                    await mqttClient.PublishAsync(result);
+                }
+            };
+        }
+    }
+}
