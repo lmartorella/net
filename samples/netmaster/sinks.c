ddfb687860eab444a8abd9c95acd172313bf171a
Luciano Martorella <mad_lux_it@yahoo.it>
2023-05-14 16:14:29 +0200

    - Migrated to lmartorella/pic-modbus

--- samples/netmaster/sinks.c
+++
@@ -1,6 +0,0 @@
-#include <net/net.h>
-
-const char* const SINK_IDS = SINK_SYS_ID;
-const uint16_t SINK_IDS_COUNT = 1;
-const SinkFunction const sink_writeHandlers[] = { sys_write_prim };
-const SinkFunction const sink_readHandlers[] = { sys_read_prim };
