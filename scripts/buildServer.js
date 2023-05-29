aa855bd9a6e98e375d81d87d6100c85d5885bdf8
Luciano Martorella <mad_lux_it@yahoo.it>
2023-05-29 22:27:54 +0200

    - Removed msbuild npm dep

--- scripts/buildServer.js
+++
@@ -1,7 +0,0 @@
-const msbuild = new (require('msbuild'))();
-const path = require('path');
-
-msbuild.sourcePath = path.join(__dirname, '../home.server.sln');
-msbuild.configuration = process.argv[2];
-msbuild.config('version','16.0');
-msbuild.build(); 
