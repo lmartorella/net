c24290d3cf9849bc6b9f2211e5295fc1008fa8c8
Luciano Martorella <mad_lux_it@yahoo.it>
2024-07-13 08:57:09 +0200

    - FIxed

--- mocks/notification-tester/SendTest.cs
+++ mocks/notification-tester/SendTest.cs
@@ -10,19 +10,24 @@ class SendTest(ILogger<SendTest> logger, MqttService mqttService) : BackgroundSe
 {
     protected async override Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        var rpcCaller = await mqttService.RegisterRpcOriginator("notification/send_mail");
         await mqttService.WaitConnected();
+        var rpcCaller = await mqttService.RegisterRpcOriginator("notification/send_mail");
 
         for (int i = 0; i < 3; i++)
         {
             logger.LogInformation("Sending test mail {0}...", i);
-            await rpcCaller.JsonRemoteCall<SendMailRequestMqttPayload, RpcVoid>(new SendMailRequestMqttPayload
+            var task = rpcCaller.JsonRemoteCall<SendMailRequestMqttPayload, RpcVoid>(new SendMailRequestMqttPayload
                 {
                     Title = "Test Mail",
                     Body = "Test Body",
                     IsAdminReport = true
                 }
             );
+            if (await Task.WhenAny(task, Task.Delay(TimeSpan.FromSeconds(5))) != task)
+            {
+                logger.LogError("TIMEOUT");
+                Environment.Exit(1);
+            }
             await Task.Delay(TimeSpan.FromSeconds(5));
             logger.LogInformation("Mail sent");
         }
